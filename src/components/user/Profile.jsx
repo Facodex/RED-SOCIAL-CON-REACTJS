143d48d848e6b5abfe54eafa94c3c8b341bbe670
Facundo Benitez <facu.cr9@gmail.com>
2023-01-07 11:25:58 -0300

    started page profile and finish page followging and followers of user

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -1,7 +1,146 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import { Link, useParams } from 'react-router-dom';
+import avatar from '../../assets/img/user.png';
+import {GetProfile} from '../../helpers/GetProfile';
+import { Global } from '../../helpers/Global';
+import useAuth from '../../hooks/useAuth';
+
 
 export const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+
+    const [user, setUser] = useState({});
+    const [counters, setCounters] = useState({});
+    const params = useParams();
+    const {auth} = useAuth();
+
+    useEffect(() => {
+        GetProfile(params.userId, setUser);
+        getCounters();
+    }, []);
+
+    useEffect(() => {
+        GetProfile(params.userId, setUser);
+        getCounters();
+    }, [params]);
+    
+    const getCounters = async () => {
+
+        const request = await fetch(Global.url + 'user/counters/' + params.userId, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            }
+        });
+
+        const data = await request.json();
+
+        if(data.following){
+            setCounters(data);
+        }
+    }
+
+
+    return (
+        <section className="layout__content">
+
+            {/* aside profile user  */}
+            <header className="aside__profile-info">
+
+                <div className="profile-info__general-info">
+                    <div className="general-info__container-avatar">
+                            {auth.image != 'default.png' ?
+                                <img src={Global.url + 'user/avatar/' + auth.image} className="container-avatar__img" alt="Foto de perfil" />
+                                :
+                                <img src={avatar} className="container-avatar__img" alt="Foto de perfil" />
+                            }
+                    </div>
+
+                    <div className="general-info__container-names">
+                        <div>
+                            <h1 href="#" className="container-names__name">{user.name} {user.surname}</h1>
+                            <button className="content__button content__button--right">Seguir</button>
+                        </div>
+                            
+                        <h2 className="container-names__nickname">{user.nick}</h2>
+                        <p>{user.bio}</p>
+                    </div>
+                </div>
+
+                <div className="profile-info__stats">
+
+                    <div className="stats__following">
+                        <Link to={"/social/siguiendo/" + user._id} className="following__link">
+                            <span className="following__title">Siguiendo</span>
+                            <span className="following__number">{counters.following >= 1 ? counters.following : 0}</span>
+                        </Link>
+                    </div>
+
+                    <div className="stats__following">
+                        <Link to={"/social/seguidores/" + user._id} className="following__link">
+                            <span className="following__title">Seguidores</span>
+                            <span className="following__number">{counters.followed >= 1 ? counters.followed : 0}</span>
+                        </Link>
+                    </div>
+
+                    <div className="stats__following">
+                        <Link to={'/social/perfil/'+user._id} className="following__link">
+                            <span className="following__title">Publicaciones</span>
+                            <span className="following__number">{counters.publications >= 1 ? counters.publications : 0}</span>
+                        </Link>
+                    </div>
+
+                </div>
+
+            </header>
+
+
+            <div className="content__posts">
+
+                <div className="posts__post">
+
+                    <div className="post__container">
+
+                        <div className="post__image-user">
+                            <a href="#" className="post__image-link">
+                                <img src={avatar} className="post__user-image" alt="Foto de perfil" />
+                            </a>
+                        </div>
+
+                        <div className="post__body">
+
+                            <div className="post__user-info">
+                                <a href="#" className="user-info__name">Victor Robles</a>
+                                <span className="user-info__divider"> | </span>
+                                <a href="#" className="user-info__create-date">Hace 1 hora</a>
+                            </div>
+
+                            <h4 className="post__content">Hola, buenos dias.</h4>
+
+                        </div>
+
+                    </div>
+
+
+                    <div className="post__buttons">
+
+                        <a href="#" className="post__button">
+                            <i className="fa-solid fa-trash-can"></i>
+                        </a>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
+            <div className="content__container-btn">
+                <button className="content__btn-more-post">
+                    Ver mas publicaciones
+                </button>
+            </div>
+
+        </section>
+    )
 }
